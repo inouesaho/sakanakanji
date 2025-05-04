e607a74f80c32b1a114ac99a83d18a9fe8aff4d5
inouesaho <saho.inouye@gmail.com>
2025-05-04 08:23:12 +0000

    GitHub Pagesへのデプロイメントワークフローを追加

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  assetsInclude: ['**/*.json']
+  assetsInclude: ['**/*.json'],
+  base: '/sakanakanji/'  // リポジトリ名を指定
 })
