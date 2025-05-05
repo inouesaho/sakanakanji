9032ea3a48df3572dae79c5b223b8c6278cd7f34
inouesaho <saho.inouye@gmail.com>
2025-05-05 07:17:00 +0000

    feat: 川魚の漢字データを追加し、JSONファイルの読み込み方法を改善

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// JSONファイル用のカスタムプラグイン
+function jsonPlugin() {
+  return {
+    name: 'json-plugin',
+    transform(code, id) {
+      if (id.endsWith('.json')) {
+        return {
+          code: `export default ${code}`,
+          map: null
+        }
+      }
+    }
+  }
+}
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), jsonPlugin()],
   assetsInclude: ['**/*.json'],
-  base: '/sakanakanji/'  // リポジトリ名を指定
+  optimizeDeps: {
+    include: [],
+    exclude: ['src/data/fish-kanji.json']
+  }
 })
